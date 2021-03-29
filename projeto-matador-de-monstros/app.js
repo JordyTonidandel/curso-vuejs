5cb79c32b1491ea6431acc315d86d423b2fabe32
Jordy Tonidandel <jordytonidandell@hotmail.com>
2021-03-29 11:18:46 -0300

    Começando o projeto matador de monstros.

---
+++ projeto-matador-de-monstros/app.js
@@ -0,0 +1,16 @@
+new Vue ({
+    el: '#app',
+    data: {
+        playerLife: 15,
+        monsterLife: 100
+    },
+    computed: {
+
+    },
+    methods: {
+        
+    },
+    watch: {
+
+    }
+})
