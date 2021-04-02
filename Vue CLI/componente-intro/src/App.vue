ee7a694186c2ad73875ef0ec32efcc1078b51497
Stella Oliveira M <stellabertthiolly@live.com>
2021-04-02 22:45:55 +0044

    iniciando desafio de componentes

---
+++ Vue CLI/componente-intro/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>{{ contadores }}</h1>
+    <app-contadores></app-contadores>
+  </div>
+</template>
+
+<script>
+export default {
+  el: '#app',
+  data() {
+    return {
+      contadores : 'Contador'
+    }
+  }
+}
+</script>
+
+<style></style>
